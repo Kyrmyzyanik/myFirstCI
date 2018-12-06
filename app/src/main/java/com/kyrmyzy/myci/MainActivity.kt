ecc586ac501c51371c1aa9cb41e23e9eeeb766d8
Kyrmyzy Kaliyeva <kyrmyzy@kfit.com>
2018-12-06 11:50:18 +0800

    test one

--- app/src/main/java/com/kyrmyzy/myci/MainActivity.kt
+++ app/src/main/java/com/kyrmyzy/myci/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kyrmyzy.myci
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.e(" -- ", " -- ");
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
